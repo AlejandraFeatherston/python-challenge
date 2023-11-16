e11706011324a108b46a3b2071f529b7aab296e1
AlejandraFeatherston <alejandra.featherston@gmail.com>
2023-11-15 22:30:14 -0500

    almost done

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,12 @@ import os
 
 budget_data_csv = "/Users/alejandrafeatherston-rajcic/Documents/GitHub/python-challenge/PyBank/Resources/budget_data.csv"
 
-totalmonths = 0
-totalprofits = 0
+summonths = 0
+sumprofits = 0
 changeprofits = 0
 changeofprofits_list = []
-greatestincrease = 0
-lowestincrease = 0
+greatestincrease = ["",0]
+greatestdecrease = ["", 999999999999]
 
 profit_loss = 0
 
@@ -18,33 +18,37 @@ with open (budget_data_csv, newline = "") as csvfile:
     next(csvreader)
 
     for row in csvreader:
-        totalmonths +=1
-        totalprofits = int(row[1])+ totalprofits
+        
+        sumprofits = int(row[1]) + sumprofits
+        summonths +=1
 
-        changeprofits = int(row[1])-profit_loss
+        changeprofits = int(row[1]) - profit_loss
         profit_loss = int(row[1])
-        changeofprofits_list.append(changeprofits)    
 
-
+        changeofprofits_list.append(changeprofits)
     
+        if changeprofits > greatestincrease[1]:
+            greatestincrease[0]=row[0]
+            greatestincrease[1]= changeprofits
+        if changeprofits < greatestdecrease[1]:
+            greatestdecrease[0]=row[0]
+            greatestdecrease[1]=changeprofits
+
 averageprofitchange = round((sum(changeofprofits_list[1:]))/(len(changeofprofits_list[1:])),2)
 
-max = max(changeofprofits_list)
-min = min(changeofprofits_list)
-
-print("Total months: " + str(totalmonths))
-print("Total: " + str(totalprofits))
+print("Total months: " + str(summonths))
+print("Total: " + str(sumprofits))
 print("Average  change: " + str(averageprofitchange))
-print("Greatest increase: " + str(max))
-print("Greatest decrease: " +str(min))
+print("Greatest increase: " + str(greatestincrease[1]))
+print("Greatest decrease: " +str(greatestdecrease[1]))
 
 
 analysis_file = "PyBank/analysis/analysis.txt"
 
 with open(analysis_file, 'w') as txtfile:
     txtfile.write("Financial Analysis\n")
-    txtfile.write("Total months: " + str(totalmonths)+"\n")
-    txtfile.write("Total: " + str(totalprofits)+"\n")
+    txtfile.write("Total months: " + str(summonths)+"\n")
+    txtfile.write("Total: " + str(sumprofits)+"\n")
     txtfile.write("Average  change: " + str(averageprofitchange)+"\n")
-    txtfile.write("Greatest increase: " + str(max)+"\n")
-    txtfile.write("Greatest decrease: " +str(min)+"\n")
+    txtfile.write("Greatest increase: " + str(greatestincrease[1])+"\n")
+    txtfile.write("Greatest decrease: " +str(greatestdecrease[1])+"\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,10 +2,10 @@ import csv
 
 votescast = 0
 candidate_list = []
-votes_per_candidate = []
-percentagevotes_per_candidate = []
-winner = ""
-candidate_info = {}
+totalvotes = []
+percentagevotes_per_candidate = 0
+percentagevotes_per_candidate_list = []
+candidate_votes = []
 
 election_data_csv = "PyPoll/Resources/election_data.csv"
 
@@ -15,20 +15,32 @@ with open(election_data_csv, newline = '') as csvfile:
 
     next(csvreader)
 
-    for row in csvreader:
+    for each_row in csvreader:
         votescast += 1
-        votes_per_candidate.append(row[2])
+        totalvotes.append(each_row[2])
 
-        if row[2] not in candidate_list:
-            candidate_list.append(row[2])
-
+        if each_row[2] not in candidate_list:
+            candidate_list.append(each_row[2])
+        
     for i in range(len(candidate_list)):
         count = 0
-        for j in range(len(votes_per_candidate)):
-            if candidate_list[i] == votes_per_candidate[j]:
-                count +=1
-        candidate_info[candidate_list[i]] = count
+        for j in range(len(totalvotes)):
+            if candidate_list[i] == totalvotes[j]:
+                count += 1
+        candidate_votes.append(count)
     
     for x in range(len(candidate_list)):
-        candidate_info[percentagevotes_per_candidate[x]]=(candidate_info[candidate_list[x]]/votescast*100)
-print(candidate_info)
+        percentagevotes_per_candidate = round(candidate_votes[x]/votescast*100,3)
+        percentagevotes_per_candidate_list.append(percentagevotes_per_candidate)
+    
+    winnerIndex = percentagevotes_per_candidate_list.index(max(percentagevotes_per_candidate_list))
+    
+    print("Election Results")
+    print("-----------------------------")
+    print("Total Votes: " + str(votescast))
+    print("-----------------------------")
+    for a in range(len(candidate_list)):
+        print(str(candidate_list[a]) + ": " + str(percentagevotes_per_candidate_list[a]) + "% (" + str(candidate_votes[a]) + ")")
+    print("-----------------------------")
+    print("Winner: " + str(candidate_list[winnerIndex]))
+    print("-----------------------------")
